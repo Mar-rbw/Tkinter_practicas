1a7a64cbf2b07d65c4fc129a5e987b5b1de86e4c
Robowa <marceli3@outlook.es>
2023-06-16 01:51:03 -0400

    Cambios varios
    Hubieron cambios varios

---
+++ entorno.py
@@ -0,0 +1,17 @@
+""" from tkinter import *
+from tkinter import ttk
+ """
+
+
+""" como documentar """
+https://www.youtube.com/watch?v=SJqANWdRG4I
+""" git colaboratibo """
+https://www.youtube.com/watch?v=mNiKeZw4M0s
+""" enterder POO para tkinter """
+https://www.youtube.com/watch?v=Y199sNkXq58&list=PLqlQ2-9ypflT5Lx864RXtJx4s7i3sxgs0&index=19&t=6s
+""" Tkinter Base de datos """
+https://www.youtube.com/watch?v=7QqDQEZ3FTI
+""" login """
+https://www.youtube.com/watch?v=-cpuxY8q_hk
+RBAC
+https://www.geeksforgeeks.org/flask-role-based-access-control/
--- tkinter_practice/Scripts/tk_practice/tkinter_1.py
+++
@@ -1,6 +0,0 @@
-from tkinter import *
-
-#Raiz
-root = Tk()
-
-root.mainloop()
